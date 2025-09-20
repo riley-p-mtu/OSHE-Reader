be664488e017f76adb9ead4676a6171dc09af78c
Owen Dodson <ojdodson@mtu.edu>
2025-09-20 17:02:01 -0400

    Started work to unzip epub
    Minizip is a library I am trying to use for unzipping the epub and getting the content file, which will let us identify the title, files containing pages, and the reading order of those pages. Once we can open those pages we can access the actual text and eventually parse it.

--- Firmware/Parsing.c
+++ Firmware/Parsing.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "miniz.h"
 
-int main(){
+int main() {
+    const char *fileName = "MobyDick.epub";	// Epub name
+    mz_zip_archive zip;	// "zip" Represents Epub
+    memset(&zip, 0, sizeof(zip));	// Clear memory for the zip
+
+	// Fail if it can't find Epub
+    if (!mz_zip_reader_init_file(&zip, fileName, 0)) {
+        printf("Failed to open %s\n", fileName);
+        return 1;
+    }
+
+    const char *targetFile = "OEBPS/content.opf";	// File path to file containing title and reading order
+
 
 }
-#include <stdio.h>
-struct
