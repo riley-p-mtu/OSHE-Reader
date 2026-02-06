2bbc3f293b6da1d7866edeee3cb1d7bf63342256
Riley Patterson <rileyp@mtu.edu>
2026-02-06 16:46:13 -0500

    Added button init

---
+++ Firmware/button_init.c
@@ -0,0 +1,25 @@
+//WIP
+int button_1_init()	// Up
+{
+
+}
+
+int button_2_init()	// Down
+{
+
+}
+
+int button_3_init()	// Right
+{
+
+}
+
+int button_4_init()	// Left
+{
+
+}
+
+int button_5_init()	// Select or Home
+{
+
+}
