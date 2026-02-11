40f3cca964493f92b48ff353926c24578d822204
Riley Patterson <rileyp@mtu.edu>
2026-02-11 15:10:27 -0500

    Update button_lvgl_test.ino

--- Firmware/button_lvgl_test/button_lvgl_test.ino
+++ Firmware/button_lvgl_test/button_lvgl_test.ino
@@ -1,3 +1,4 @@
+
 // Number for button pins
 const int upBtn = 5;
 const int downBtn = 11;
@@ -20,41 +21,27 @@ void setup(){
   pinMode(leftBtn, INPUT);
   pinMode(homeBtn, INPUT);
 
+// Create a group and add widgets to it so they can be selected by the keypad
+  lv_group_t *g = lv_group_create();
+
   lv_indev_t *indev = lv_indev_create();
-  lv_indev_set_type(indev, LV_INDEX_TYPE_BUTTON);
-  lv_indev_set_read_cb(indev, my_input_read);
+  lv_indev_set_type(indev, LV_INDEV_TYPE_KEYPAD);
+  lv_indev_set_read_cb(indev, keyboard_read);
+  lv_indev_set_group(indev, g);
 }
 
-static void btn_event_cb(lv_event_t * e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * btn = lv_event_get_target_obj(e);
-    if(code == LV_EVENT_CLICKED) {
-        static uint8_t cnt = 0;
-        cnt++;
-
-        /*Get the first child of the button which is the label and change its text*/
-        lv_obj_t * label = lv_obj_get_child(btn, 0);
-        lv_label_set_text_fmt(label, "Button: %d", cnt);
-    }
-}
-
-/**
- * Create a button with a label and react on click event.
- */
-void lv_example_get_started_2(void)
-{
-    lv_obj_t * btn = lv_button_create(lv_screen_active());     /*Add a button the current screen*/
-    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
-    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
-    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
-
-    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
-    lv_label_set_text(label, "Button");                     /*Set the labels text*/
-    lv_obj_center(label);
+void keyboard_read(lv_indev_t *indev, lv_indev_data_t *data) {
+  if(key_pressed()) {
+    data->key = my_last_key();            /* Get the last pressed or released key */
+    data->state = LV_INDEV_STATE_PRESSED;
+  } else {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
 }
 
 void loop(){
+
+
   uint8_t my_input_read(void)
   {
     if (upBtnState == LOW && digitalRead(upBtn)) {
